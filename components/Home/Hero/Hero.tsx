03fee0e8f21b6dca5df9d55d1e06e8691ffaa826
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-04-27 15:35:05 +0600

    Updated banner or hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -7,19 +7,20 @@ const Hero = () => {
   return (
     <div className="container mx-auto px-4 xl:px-0 xl:py-4 text-white">
       <div
-        className="h-[100vh] bg-cover bg-center rounded-t-xl flex relative "
+        className="h-[100vh] bg-cover bg-center rounded-t-xl flex relative overflow-hidden "
         style={{
           backgroundImage: `url(${bannerbackground.src})`,
         }}
       >
-        <div className="max-w-[1320px] mx-auto flex flex-row-reverse justify-between items-center ">
+        <div className="max-w-[1320px] mx-auto flex flex-row-reverse justify-between items-center  ">
           <div className="absolute right-0 ">
             <img
               src={gamecharecter.src}
               alt=""
-              className="aspect-square xl:h-[800px]"
+              className="aspect-square xl:h-[800px] z-10"
             />
           </div>
+          <div className="h-32 w-full bg-[#0e0c15] absolute bottom-0 right-0 blur-xl"></div>
           <div className="relative ">
             <div className="-ml-[30%]">
               <span className="text-[1.5rem] font-medium bg-gradient-to-l to-[#FADA1B] via-[#fad81bc0] from-white bg-clip-text text-transparent ">
@@ -27,8 +28,18 @@ const Hero = () => {
               </span>
               <h1 className="text-[4.5rem] font-bold leading-[110%] max-w-[19ch]">
                 Buy Your Favorite In-Game Items{" "}
-                  Instantly, Securely, and Easily 
-                 with Blox Fruit Hub!
+                <span className="bg-gradient-to-l to-[#FADA1B] from-white bg-clip-text text-transparent ">
+                  Instantly
+                </span>
+                ,{" "}
+                <span className="bg-[#FADA1B] bg-clip-text text-transparent ">
+                  Securely
+                </span>
+                ,{" "}
+                <span className="bg-gradient-to-l to-[#FADA1B] from-white bg-clip-text text-transparent ">
+                  and Easily{" "}
+                </span>
+                with Blox Fruit Hub!
               </h1>
               <p className="max-w-[60ch]">
                 Looking for fast and trusted item purchases in BloxFruits, Blue
