1cc3894903c6fc9f32de9e39811c239f75a94e9d
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-05-01 14:34:18 +0600

    Footer Responsive added

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -9,42 +9,42 @@ import Image from "next/image";
 import logo from "@/public/logo.svg";
 const Footer = () => {
   return (
-    <footer className=" text-white px-6 py-10 mt-8 border-t border-[#e4e4e414]">
+    <footer className=" text-white px-4 py-10  border-t border-[#e4e4e414]">
       <div className="container mx-auto">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mb-6 lg:mb-8">
           <div className="flex items-center space-x-2 mb-4">
             <Image src={logo} alt="logo"  width={183} height={52} className="" />
           </div>
-          <div className="flex space-x-4 text-lg text-white">
-            <FaFacebook />
-            <FaInstagram />
-            <FaTwitter />
-            <FaDiscord />
+          <div className="flex space-x-3 text-lg text-white">
+            <a href="#"><FaFacebook size={24} /></a>
+            <a href="#"><FaInstagram size={24} /></a>
+            <a href="#"><FaTwitter size={24} /></a>
+            <a href="#"><FaDiscord size={24} /></a>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
           {/* Logo + Newsletter */}
           <div>
-            <p className="mb-3">Don’t miss our latest News</p>
-            <form className="flex">
+            <p className="mb-3 text-[1.5rem]">Don’t miss our latest News</p>
+            <form className="flex relative">
               <input
                 type="email"
                 placeholder="Email address"
-                className="bg-[#191a23] text-white px-4 py-2 rounded-l-md w-full outline-none"
+                className="bg-[#15131d] text-white p-3 rounded-lg w-full outline-none"
               />
               <button
                 type="submit"
-                className="bg-yellow-400 text-black px-4 py-2 rounded-r-md font-semibold"
+                className="absolute right-2 top-1/2 -translate-y-1/2 text-sm bg-gradient-to-l to-[#FADA1B] from-[#FFF] text-black px-4 py-2 rounded-sm font-semibold cursor-pointer"
               >
                 Subscribe
               </button>
             </form>
-            <div className="text-xs text-gray-400 mt-4">
-              <p>
-                <span className="font-bold">GAMIA IS A PROJECT FROM</span>{" "}
-                <span className="text-yellow-400">INOVATION INC.</span>
+            <div className="text-xs text-[#F0F0F0] mt-4">
+              <p className="mt-12 mb-6 ">
+                <span className="">GAMIA IS A PROJECT FROM</span>{" "}
+                <span className="bg-gradient-to-l to-[#FADA1B] from-[#FFF] bg-clip-text text-transparent">INOVATION INC.</span>
               </p>
-              <p className="mt-2">
+              <p className="text-sm ">
                 Nothing on this website constitutes financial advice. Always
                 consult a qualified financial advisor before making purchases.
               </p>
@@ -54,7 +54,7 @@ const Footer = () => {
           {/* Social Media */}
           <div>
             <h3 className="font-semibold mb-3">Social Media</h3>
-            <ul className="space-y-2 text-gray-300">
+            <ul className="space-y-8 text-gray-300">
               <li>
                 <a href="#">Discord</a>
               </li>
@@ -70,7 +70,7 @@ const Footer = () => {
           {/* All Pages */}
           <div>
             <h3 className="font-semibold mb-3">All Pages</h3>
-            <ul className="space-y-2 text-gray-300">
+            <ul className="space-y-8 text-gray-300">
               <li>
                 <a href="#">Home</a>
               </li>
@@ -86,7 +86,7 @@ const Footer = () => {
           {/* Products */}
           <div>
             <h3 className="font-semibold mb-3">Products</h3>
-            <ul className="space-y-2 text-gray-300">
+            <ul className="space-y-8 text-gray-300">
               <li>
                 <a href="#">Blox Fruits</a>
               </li>
