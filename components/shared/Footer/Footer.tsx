442ff5d4c876a9c476d65fd9fcf350815a2a25b4
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-05-01 14:53:16 +0600

    Updated footer

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -1,25 +1,28 @@
 import React from "react";
-import {
-  FaDiscord,
-  FaInstagram,
-  FaTwitter,
-  FaFacebook,
-} from "react-icons/fa";
+import { FaDiscord, FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
 import Image from "next/image";
 import logo from "@/public/logo.svg";
 const Footer = () => {
   return (
-    <footer className=" text-white px-4 py-10  border-t border-[#e4e4e414]">
-      <div className="container mx-auto">
+    <footer className=" text-white  border-t border-[#e4e4e414]">
+      <div className="container mx-auto px-4 2xl:px-0 py-10">
         <div className="flex justify-between items-center mb-6 lg:mb-8">
           <div className="flex items-center space-x-2 mb-4">
-            <Image src={logo} alt="logo"  width={183} height={52} className="" />
+            <Image src={logo} alt="logo" width={183} height={52} className="" />
           </div>
           <div className="flex space-x-3 text-lg text-white">
-            <a href="#"><FaFacebook size={24} /></a>
-            <a href="#"><FaInstagram size={24} /></a>
-            <a href="#"><FaTwitter size={24} /></a>
-            <a href="#"><FaDiscord size={24} /></a>
+            <a href="#">
+              <FaFacebook size={24} />
+            </a>
+            <a href="#">
+              <FaInstagram size={24} />
+            </a>
+            <a href="#">
+              <FaTwitter size={24} />
+            </a>
+            <a href="#">
+              <FaDiscord size={24} />
+            </a>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
@@ -42,7 +45,9 @@ const Footer = () => {
             <div className="text-xs text-[#F0F0F0] mt-4">
               <p className="mt-12 mb-6 ">
                 <span className="">GAMIA IS A PROJECT FROM</span>{" "}
-                <span className="bg-gradient-to-l to-[#FADA1B] from-[#FFF] bg-clip-text text-transparent">INOVATION INC.</span>
+                <span className="bg-gradient-to-l to-[#FADA1B] from-[#FFF] bg-clip-text text-transparent">
+                  INOVATION INC.
+                </span>
               </p>
               <p className="text-sm ">
                 Nothing on this website constitutes financial advice. Always
@@ -108,13 +113,15 @@ const Footer = () => {
       </div>
 
       {/* Bottom Footer */}
-      <div className="border-t border-[#e4e4e414] mt-10 pt-5 flex flex-col md:flex-row justify-between items-center text-sm text-gray-500">
-        <div className="space-x-4 mb-3 md:mb-0">
-          <a href="#">Privacy Policy</a>
-          <a href="#">TOS</a>
-          <a href="#">Cookies Policy</a>
+      <div className="border-t border-[#e4e4e414] mt-10 text-sm text-gray-500">
+        <div className="container mx-auto py-5 flex flex-col md:flex-row justify-between items-center ">
+          <div className="space-x-4 mb-3 md:mb-0">
+            <a href="#">Privacy Policy</a>
+            <a href="#">TOS</a>
+            <a href="#">Cookies Policy</a>
+          </div>
+          <p className="mt-4 md:mt-0">© 2025 Rivals. All rights reserved.</p>
         </div>
-        <p className="mt-4 md:mt-0">© 2025 Rivals. All rights reserved.</p>
       </div>
     </footer>
   );
