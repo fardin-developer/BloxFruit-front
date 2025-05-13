05eac059b78979c604e30d03eb9d608f0576d7a1
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-05-13 16:58:10 +0600

    Dynamic table added

--- components/Dashboard/Products/ProductsList.tsx
+++ components/Dashboard/Products/ProductsList.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+"use client";
+import DynamicTable, {
+  TableColumn,
+} from "@/components/ui/DynamicTable/DynamicTable";
+import React from "react";
+
+const products = [
+  {
+    image_url: "",
+    name: "Product 1",
+    clinic_count: 10,
+    created_at: "2021-01-01 12:00:00",
+  },
+  {
+    image_url: "",
+    name: "Product 2",
+    clinic_count: 20,
+    created_at: "2021-01-01 12:00:00",
+  },
+  {
+    image_url: "",
+    name: "Product 3",
+    clinic_count: 30,
+    created_at: "2021-01-01 12:00:00",
+  },
+];
+
 
 const ProductsList = () => {
+  const columns: TableColumn[] = [
+    { key: "image_url", label: "Image", type: "image" },
+    { key: "name", label: "Procedures Name", type: "text" },
+    { key: "clinic_count", label: "Clinics", type: "text" },
+    { key: "created_at", label: "Join Date & Time", type: "text" },
+  ];
+
+  const tableActions = (row: any) => {
     return (
-        <div>
-            <h1>Products List</h1>
-        </div>
+      <div>
+        <button>Edit</button>
+      </div>
     );
+  };
+  return (
+    <div>
+      <h1>Products List</h1>
+      <DynamicTable columns={columns} data={products} actions={tableActions} />
+    </div>
+  );
 };
 
 export default ProductsList;
