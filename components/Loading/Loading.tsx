9d43f1e0d97bbaf86052eba7a56f340c90a2ba2f
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-06-03 12:28:10 +0600

    Updated loading

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -5,8 +5,8 @@ const Loading = () => {
     <div>
       <div className="flex items-center justify-center py-10 relative ">
         <div className=" animate-spin relative">
-          <div className="rounded h-8 w-8 border border-[#fada1d]"></div>
-          <div className="rounded h-8 w-8 border border-[#e7e7ad] rotate-45 absolute top-0"></div>
+          <div className="rounded h-6 w-6 border border-[#fada1d]"></div>
+          <div className="rounded h-6 w-6 border border-[#e7e7ad] rotate-45 absolute top-0"></div>
         </div>
       </div>
     </div>
