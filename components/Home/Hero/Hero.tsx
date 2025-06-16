a70cf4a62c6e054647cc45d7078710a3a4440124
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-06-16 16:28:39 +0600

    Hero updated

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,28 +1,29 @@
 "use client";
 import React from "react";
-import bannerbackground from "@/public/images/bannerbackground.png";
+import bannerbackground from "@/public/images/hero.svg";
 import gamecharecter from "@/public/images/gamecharecter.png";
 import Image from "next/image";
 const Hero = () => {
   return (
-    <div className="max-w-[1616px] mx-auto px-4  xl:py-4 text-white">
+    <div className="max-w-[1616px] mx-auto px-4  xl:py-4 text-white mt-4 xl:mt-0">
       <div
-        className="h-[100vh] bg-cover bg-center rounded-t-xl flex relative overflow-hidden "
+        className="h-[64vh] xl:h-[100vh] bg-cover bg-center rounded-t-xl flex relative overflow-hidden "
         style={{
           backgroundImage: `url(${bannerbackground.src})`,
         }}
       >
+        <div className="absolute top-0 left-0 w-full h-full bg-[#0e0c15]/90"></div>
         <div className="max-w-[1320px] mx-auto flex flex-row-reverse justify-between items-center  ">
           <div className="absolute right-0 ">
             <img
               src={gamecharecter.src}
               alt="gamecharecter"
-              className="aspect-square xl:h-[600px] 2xl:h-[800px] z-10"
+              className="hidden lg:block aspect-square lg:h-[500px] xl:h-[600px] 2xl:h-[800px] z-10"
             />
           </div>
-          <div className="h-32 w-full bg-[#0e0c15] absolute bottom-0 right-0 blur-sm"></div>
+          <div className="h-32 w-full bg-[#0e0c15] absolute -bottom-10 xl:-bottom-5 right-0 blur-sm"></div>
           <div className="relative ">
-            <div className="xl:-ml-[22.9%] 2xl:-ml-[32.9%]">
+            <div className="lg:-ml-[22.9%] 2xl:-ml-[32.9%] px-4 xl:px-0">
               <span className="text-[1.2rem]  xl:text-[1.5rem] font-medium bg-gradient-to-l to-[#FADA1B] via-[#fad81bc0] from-white bg-clip-text text-transparent ">
                 Unlock Exclusive Roblox Gear in Seconds!
               </span>
