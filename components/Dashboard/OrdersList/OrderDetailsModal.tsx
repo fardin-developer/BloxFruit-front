a5ec5340e56c45e12e5eaad7ffafbec2964ece93
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-06-28 11:14:30 +0600

    Details modal completed

---
+++ components/Dashboard/OrdersList/OrderDetailsModal.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { IoClose } from "react-icons/io5";
+
+interface OrderDetailsModalProps {
+  order: any;
+  onClose: () => void;
+}
+
+const OrderDetailsModal: React.FC<OrderDetailsModalProps> = ({
+  order,
+  onClose,
+}) => {
+  const orderItems = order?.items;
+
+  return (
+    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50 ">
+      <div className="bg-[#080705] p-6 rounded-lg shadow-lg max-w-4xl w-full relative border border-[#fada1d]">
+        <h2 className="text-xl font-bold text-center uppercase text-[#fada1d]">
+          Order Details
+        </h2>
+        <div>
+          <p className="text-xl font-bold">
+            <strong className="text-[#fada1d]">Roblox Username:</strong>{" "}
+            {order?.customer_name}
+          </p>
+          {/* Order Items */}
+          <div className="">
+            <h3 className="text-lg font-bold uppercase text-[#fada1d] my-4">
+              Order Items
+            </h3>
+            <div className="grid grid-cols-1 md:grid-cols-2  gap-4 h-72 overflow-y-auto custom-scroll">
+              {orderItems.map((item: any) => (
+                <div key={item.id}>
+                  <div className="bg-[#fada1d] text-black flex gap-2 border border-[#fada1d]">
+                    <div className="bg-[#080705] p-2 flex items-center justify-center">
+                      <img
+                        src="/mainCardImages/common.png"
+                        alt=""
+                        className="w-16 h-16 object-cover"
+                      />
+                    </div>
+                    <div>
+                      <p>
+                        Game Name :{" "}
+                        <span className=" font-bold capitalize">
+                          {item.product.games_name}
+                        </span>
+                      </p>
+                      <p>
+                        Rarity :{" "}
+                        <span className=" font-bold capitalize">
+                          {item.product.type}
+                        </span>
+                      </p>
+                      <p>
+                        Quantity :{" "}
+                        <span className=" font-bold text-center">
+                          {item.quantity}
+                        </span>
+                      </p>
+                      <p>
+                        Price :{" "}
+                        <span className=" font-bold text-center">
+                          ₹{item.product.regularPrice}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="mt-4">
+            <p className="text-center text-sm font-bold text-gray-500">
+              <strong className="text-red-500">Note:</strong> Once all the
+              products have been delivered, click the 'Complete' button.{" "}
+            </p>
+            <button
+              className={`bg-[#fada1d] mt-4 text-black font-bold px-4 py-2 hover:brightness-150 duration-300 cursor-pointer ${
+                order?.order_delivery === "Pending"
+                  ? "bg-red-500 text-white"
+                  : "bg-green-500"
+              }`}
+            >
+              {order?.order_delivery === "Pending"
+                ? "Complete Order"
+                : "Order Completed"}
+            </button>
+          </div>
+        </div>
+        <button
+          onClick={onClose}
+          className="bg-red-500 absolute top-5 right-5 text-white p-1 md:p-2 cursor-pointer"
+        >
+          <IoClose size={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetailsModal;
--- components/Dashboard/OrdersList/OrdersList.tsx
+++ components/Dashboard/OrdersList/OrdersList.tsx
@@ -3,11 +3,8 @@ import DynamicTable, {
   TableColumn,
 } from "@/components/ui/DynamicTable/DynamicTable";
 import React, { useState } from "react";
-import image from "@/public/mainCardImages/legendary.png";
-import image2 from "@/public/mainCardImages/common.png";
-import image3 from "@/public/mainCardImages/uncommon.png";
-import { MdCheckCircle } from "react-icons/md";
 import { useGetOrdersQuery } from "@/app/store/api/services/orderApi";
+import OrderDetailsModal from "./OrderDetailsModal"; 
 
 const OrdersList = () => {
   const { data: orders, isLoading } = useGetOrdersQuery(null);
@@ -15,21 +12,7 @@ const OrdersList = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedOrder, setSelectedOrder] = useState<any>(null);
 
-  const items = ordersData?.map((item: any) => {
-    return {
-      customer_email: item.customer_email,
-      customer_name: item.customer_name,
-      customer_phone: item.customer_phone,
-      created_at: new Date(item.created_at).toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-      status: item.status,
-    };
-  });
+  console.log("ordersData", ordersData);
 
   const formatStatus = (status: string) => {
     if (status === "PENDING") {
@@ -41,17 +24,21 @@ const OrdersList = () => {
     } else if (status === "COMPLETED") {
       return (
         <p className="bg-green-500 text-black font-bold px-4 py-2 text-center">
-          Completed
+          Paid
         </p>
       );
     } else if (status === "completed") {
       return (
         <p className="bg-green-500 text-black font-bold px-4 py-2 text-center">
-          Completed
+          Paid
         </p>
       );
     }
     return status;
+  };
+
+  const formatAmount = (amount: number) => {
+    return `₹${amount}`;
   };
 
   const formatData = (data: any) => {
@@ -59,6 +46,7 @@ const OrdersList = () => {
     return data.map((item: any) => ({
       ...item,
       status: formatStatus(item.status),
+      amount: formatAmount(item.amount),
     }));
   };
 
@@ -67,7 +55,8 @@ const OrdersList = () => {
     { key: "customer_name", label: "Roblox Username", type: "text" },
     { key: "customer_phone", label: "Customer Phone", type: "text" },
     { key: "created_at", label: "Order Date & Time", type: "text" },
-    { key: "status", label: "Status", type: "text" },
+    { key: "amount", label: "Total Amount", type: "text" },
+    { key: "status", label: "Payment Status", type: "text" },
   ];
 
   const tableActions = (row: any) => {
@@ -86,25 +75,6 @@ const OrdersList = () => {
     );
   };
 
-  const Modal = ({ order, onClose }: { order: any; onClose: () => void }) => (
-    <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-xl font-bold">Order Details</h2>
-        <p><strong>Email:</strong> {order.customer_email}</p>
-        <p><strong>Username:</strong> {order.customer_name}</p>
-        <p><strong>Phone:</strong> {order.customer_phone}</p>
-        <p><strong>Order Date:</strong> {order.created_at}</p>
-        <p><strong>Status:</strong> {order.status}</p>
-        <button
-          onClick={onClose}
-          className="bg-red-500 text-white py-1 px-4 mt-4 rounded"
-        >
-          Close
-        </button>
-      </div>
-    </div>
-  );
-
   return (
     <div className="">
       <h1 className="text-2xl font-bold mb-4">Orders List</h1>
@@ -115,9 +85,8 @@ const OrdersList = () => {
         loading={isLoading}
       />
 
-      {/* Conditionally render the modal */}
       {isModalOpen && selectedOrder && (
-        <Modal order={selectedOrder} onClose={() => setIsModalOpen(false)} />
+        <OrderDetailsModal order={selectedOrder} onClose={() => setIsModalOpen(false)} />
       )}
     </div>
   );
